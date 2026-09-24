9cef933130759ab97f73bb412cc053cf90f33149
shabihosama@gmail.com <shab>
2026-09-24 19:06:13 +0530

    setup.py-ready

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Read requirements.txt and return the list of dependencies.
+    """
+    requirements = []
+
+    with open(file_path, "r") as file_obj:
+        requirements = file_obj.readlines()
+
+    requirements = [req.strip() for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Osama-Shabih",
+    author_email="Osamashabih80@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
